ab4b0b6a493e7b9422993a86b868bcc9195891d1
Amol Dhamale <amoldhamale1105@gmail.com>
2021-04-09 20:13:05 +0530

    Playing with pointer references. Better than having print hello world as a sample code file!

--- samplecode.cpp
+++ samplecode.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
 
+int global_var = 20;
+
+void modify_value(int*&);
+
 int main()
 {
-	std::cout<<"Hello World!"<<std::endl;
-	return 0;
+	int a = 10;
+	int *p = &a;
+	std::cout<<"Hello! Playing with pointer references"<<std::endl;
+	std::cout<<"pointer value: "<<p<<std::endl;
+	modify_value(p);
+	std::cout<<"pointer value after func call: "<<p<<std::endl;
+	std::cout<<"value held by pointer: "<<*p<<std::endl;
+}
+
+void modify_value(int*& ptr)
+{
+	std::cout<<"ptr value: "<<ptr<<std::endl;
+	std::cout<<"value held by ptr before assignment: "<<*ptr<<std::endl;
+	ptr = &global_var;
+	std::cout<<"ptr value after assignment: "<<ptr<<std::endl;
+	std::cout<<"value held by ptr: "<<*ptr<<std::endl;
 }
