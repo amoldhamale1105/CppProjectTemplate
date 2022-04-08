f08501ee581e07c0c80a6f94547cc4e6435c6371
Amol Dhamale <amoldhamale1105@gmail.com>
2022-04-09 00:35:21 +0530

    linking external custom ds lib and testing basic int vector container

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <Sample.hpp>
+#include <Vector.hpp>
 
 int global_var = 20;
 
@@ -12,6 +13,22 @@ int main(int argc, char** argv)
 	modify_value(p);
 	std::cout<<"pointer value after func call: "<<p<<std::endl;
 	std::cout<<"value held by pointer: "<<*p<<std::endl;
+
+	Vector test(8, 3);
+	std::cout<<test.capacity()<<test.size()<<std::endl;
+	test.push_back(9);
+	test.push_back(32);
+	test.push_back(76);
+	test.push_back(21);
+	test.push_back(51);
+	std::cout<<test.pop_back()<<std::endl;
+
+	for(int i = 0; i < test.size(); ++i)
+	{
+		std::cout<<test.at(i)<<" ";
+	}
+	std::cout<<std::endl;
+	std::cout<<test.capacity()<<test.size()<<std::endl;
 
     return 0;
 }
