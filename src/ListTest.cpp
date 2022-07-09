37d97d7c3618265eab9989a57a14d9673dbf56fa
Amol Dhamale <amoldhamale1105@gmail.com>
2022-07-09 18:19:42 +0530

    add more list tests to test new functionality and corner cases

--- src/ListTest.cpp
+++ src/ListTest.cpp
@@ -7,9 +7,11 @@ void basicListTest()
     myList.push_front(7);
     myList.push_front(65);
     myList.push_back(33);
+    myList.push_back(19);
     int a{52}, b{74};
     myList.push_back(a); //lvalue test
     myList.push_front(b); //lvalue test
+    myList.push_front(5);
 
     Vector<int> elementList = myList.get_list();
     for(auto i = 0; i < elementList.size(); ++i)
@@ -25,6 +27,8 @@ void basicListTest()
     strList.push_front("hello");
     std::string insertionStr("chai");
     strList.insert(insertionStr, 2);
+    strList.push_back("jaldi");
+    strList.push_front("surrrr");
 
     Vector<std::string> stringList = strList.get_list();
     for(auto i = 0; i < stringList.size(); ++i)
@@ -42,6 +46,48 @@ void basicListTest()
     std::cout<<myList.recursive_search(54)<<std::endl;
     std::cout<<strList.recursive_search("pee")<<std::endl;
 
+    myList.remove(2);
+    myList.remove(100);
+    Vector<int> modifiedList = myList.get_list();
+    for(auto i = 0; i < modifiedList.size(); ++i)
+    {
+        std::cout<<modifiedList.at(i)<<"->";
+    }
+    std::cout<<"end of list"<<std::endl;
+
+    strList.remove(-21);
+    Vector<std::string> modifiedStringList = strList.get_list();
+    for(auto i = 0; i < modifiedStringList.size(); ++i)
+    {
+        std::cout<<modifiedStringList[i]<<" ";
+    }
+    std::cout<<std::endl;
+
+    std::cout<<"front popped value in int list: "<<myList.pop_front()<<std::endl;
+    std::cout<<"front popped value in string list: "<<strList.pop_front()<<std::endl;
+    std::cout<<"back popped value in int list: "<<myList.pop_back()<<std::endl;
+    std::cout<<"back popped value in string list: "<<strList.pop_back()<<std::endl;
+    Vector<int> newList = myList.get_list();
+    for(auto i = 0; i < newList.size(); ++i)
+    {
+        std::cout<<newList.at(i)<<"->";
+    }
+    std::cout<<"end of list"<<std::endl;
+    
+    Vector<std::string> newStringList = strList.get_list();
+    for(auto i = 0; i < newStringList.size(); ++i)
+    {
+        std::cout<<newStringList[i]<<" ";
+    }
+    std::cout<<std::endl;
+
+    //Empty list tests
+    List<char> charList;
+    charList.remove(2);
+    charList.insert('G', 5);
+    charList.push_back('M');
+    std::cout<<charList.pop_front()<<std::endl;
+
     std::cout<<"--------------ending List test-----------------\n"<<std::endl;
     
-}
+};
