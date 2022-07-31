74e2389f90a56e4a422ead389e940dbbffcaeb89
Amol Dhamale <amoldhamale1105@gmail.com>
2022-07-31 13:40:29 +0530

    add stack copying tests

--- src/StackTest.cpp
+++ src/StackTest.cpp
@@ -15,8 +15,46 @@ void basicStackTest()
     books.push("Sapiens");
     std::cout<<"Topmost book: "<<books.top()<<std::endl;
 
+    Stack<std::string> moreBooks = books;
+    moreBooks.push("Blueprint");
+    while (!moreBooks.isEmpty())
+    {
+        std::cout<<moreBooks.pop()<<" ";
+    }
+    std::cout<<std::endl;
+    std::cout<<books.top()<<" "<<moreBooks.isEmpty()<<std::endl;
+
     books.clear();
     std::cout<<"Topmost book: "<<books.top()<<std::endl;
 
-    std::cout<<"--------------starting Stack test-----------------"<<std::endl;
+    struct Dict
+    {
+        const char* key;
+        uint16_t value;
+    };
+
+    Stack<Dict> myDict;
+    myDict.push({"red", 0});
+    myDict.push({"green", 1});
+    myDict.push({"blue", 2});
+
+    Stack<Dict> copyDict;
+    copyDict = myDict;
+
+    while (!myDict.isEmpty())
+    {
+        Dict instance = myDict.pop();
+        std::cout<<instance.key<<" "<<instance.value<<",";
+    }
+    std::cout<<std::endl;
+
+    copyDict.push({"alpha", 3});
+    while (!copyDict.isEmpty())
+    {
+        Dict instance = copyDict.pop();
+        std::cout<<instance.key<<" "<<instance.value<<",";
+    }
+    std::cout<<std::endl;
+    
+    std::cout<<"--------------ending Stack test-----------------\n"<<std::endl;
 }
