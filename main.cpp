22378670f536307288dcab0262ef32ac8db3fc4e
Amol Dhamale <amoldhamale1105@gmail.com>
2022-09-24 12:42:12 +0530

    improving template structure, aspects and sample code

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
-#include <Sample.hpp>
-
-int global_var = 20;
+#include <App.hpp>
 
 int main(int argc, char** argv)
 {
-	int a = 10;
-	int *p = &a;
-	std::cout<<"Hello! Playing with pointer references"<<std::endl;
-	std::cout<<"pointer value: "<<p<<std::endl;
-	modify_value(p);
-	std::cout<<"pointer value after func call: "<<p<<std::endl;
-	std::cout<<"value held by pointer: "<<*p<<std::endl;
+	std::string username;
+	if (argc > 1)
+		username = argv[1];
+	else{
+		std::cout<<"Enter your name: ";
+		std::cin >> username;
+	}
+
+	App app(username);
+	app.greet();
 
     return 0;
 }
