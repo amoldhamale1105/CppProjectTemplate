158e8b6c38f03e90eb93d42a684743e71dd7065d
Amol Dhamale <amoldhamale1105@gmail.com>
2022-12-03 21:24:38 +0530

    perform basic sanity tests on newly implemented hash map methods

--- src/HashmapTest.cpp
+++ src/HashmapTest.cpp
@@ -4,6 +4,24 @@ void basicHashmapTest()
 {
     std::cout<<"--------------starting Hashmap test-----------------"<<std::endl;
     Hashmap<std::string,int> myMap(53);
+    myMap.insert("hello", 12);
+    myMap.insert("friends", 13);
+    myMap.insert("chai", 14);
+    myMap.insert("pee", 15);
+    myMap.insert("lo", 16);
+
+    std::cout<<myMap["pee"]<<" "<<myMap.at("friends")<<std::endl;
+    myMap["friends"] = 67;
+    std::cout<<myMap.at("pee")<<" "<<myMap["friends"]<<std::endl;
+    std::cout<<myMap.find(16)<<", is empty? "<<(myMap.isEmpty() ? "yes" : "no")<<std::endl;
+
+    Vector<std::string> keys = myMap.keys();
+    while (!keys.isEmpty())
+    {
+        std::cout<<keys.pop_back()<<",";
+    }
+    std::cout<<'\b'<<std::endl;
+    
 
     std::cout<<"--------------ending Hashmap test-----------------\n"<<std::endl;
 }
